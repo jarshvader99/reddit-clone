9f46c0ee577cda762bc0700a1b4c6de98b8daf1c
Josh Harrison <jarshvader@gmail.com>
2021-01-14 22:57:57 -0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
+import React, { useEffect, useState } from "react";
+import Post from "./components/post";
+import db from "./lib/firebase";
+import Navbar from "./components/navbar";
+
+
+require('dotenv').config();
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Hook to handle the initial fetching of posts
+
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((querySnapshot) => {
+        const _posts = [];
+
+        querySnapshot.forEach((doc) => {
+          _posts.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+
+        setPosts(_posts);
+      });
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Container maxW="md" centerContent p={8}>
+        <VStack spacing={8} w="100%">
+          {posts.map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
+        </VStack>
+      </Container>
+    </>
+  );
+};
+
+export default App;
